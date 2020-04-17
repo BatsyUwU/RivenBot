afee03837ab9d0d5eedaeb7690a139e3dd84768e
XRzky <marizky65@gmail.com>
2020-04-18 03:06:48 +0700

    commands: added avatar

---
+++ src/modules/commands/information/avatar.js
@@ -0,0 +1,31 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+
+module.exports = {
+    config: {
+        name: "avatar",
+        aliases: ["av", "ava", "avy"],
+        category: "information",
+        description: "Sends the mentioned user's avatar.",
+        usage: "[mention]",
+        example: "@Rygent",
+        accessableby: "Members"
+    },
+    run: async (bot, message) => {
+        let msg = await message.channel.send("Generating...");
+        let target = message.mentions.users.first() || message.author;
+        msg.delete();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+        
+        let avatarEmbed = new MessageEmbed()
+            .setColor(roleColor === '#000000' ? Colors.CUSTOM : roleColor)
+            .setTitle(`🖼️ ${target.tag}'s avatar`)
+            .setDescription(`🔗 **[Original Resolution](${target.displayAvatarURL({ format: "png", dynamic: true, size: 2048 })})**`)
+            .setImage(target.displayAvatarURL({ format: "png", dynamic: true, size: 512 }))
+            .setFooter(`Requested by ${message.member.user.tag}`, message.member.user.avatarURL())
+            .setTimestamp();
+
+        message.channel.send(avatarEmbed);
+    }
+};
