87e6dd393dcf0675f48905839f2523347fba2cd1
XRzky <marizky65@gmail.com>
2020-04-27 05:56:05 +0700

    commands: added setavatar
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/owner/setavatar.js
@@ -0,0 +1,31 @@
+const { Access } = require("../../../utils/configs/settings");
+const Errors = require("../../../utils/functions/errors");
+
+module.exports = {
+    config: {
+        name: "setavatar",
+        aliases: ["sa", "setav"],
+        category: "owner",
+        description: "Sets the avatar of the Bots Account",
+        usage: "<link>",
+        example: "send image",
+        accessableby: "Owner"
+    },
+    run: async (bot, message, args) => {
+        if (message.deletable) {
+            message.delete();
+        }
+
+        if(message.author.id != Access.OWNERS) {
+            return Errors.ownerAccess(message);
+        }
+
+        if (!args || args.length < 1) {
+            return Errors.wrongText(message, "Please provide me with a valid link to set my avatar.");
+        }
+
+        bot.user.setAvatar(args.join(" "));
+
+        message.channel.send("Profile photo has been changed!").then((m) => m.delete({ timeout: 5000 }));
+    }
+};
