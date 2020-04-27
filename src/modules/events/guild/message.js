aff2f126cfe630d9cf0d59efc2cdbbf1d890a0b1
XRzky <marizky65@gmail.com>
2020-04-27 08:42:16 +0700

    event: message: delete suffix
    Because suffix same as args
    
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/events/guild/message.js
+++ src/modules/events/guild/message.js
@@ -5,7 +5,6 @@ module.exports = async (bot, message) => {
         return;
     };
 
-    let suffix = message.content.substr(message.content.split(" ")[0].length + 1);
     let args = message.content.slice(Client.PREFIX.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
@@ -15,6 +14,6 @@ module.exports = async (bot, message) => {
 
     let commandfile = bot.commands.get(cmd) || bot.commands.get(bot.aliases.get(cmd));
     if(commandfile) {
-        commandfile.run(bot, message, args, suffix);
+        commandfile.run(bot, message, args);
     }
 };
