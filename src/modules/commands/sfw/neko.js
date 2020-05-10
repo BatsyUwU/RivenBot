4ab25dbab43e31b09bd1072488f52ba857f59c09
XRzky <marizky65@gmail.com>
2020-05-10 13:21:23 +0700

    commands: added neko
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/sfw/neko.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "neko",
+        aliases: [""],
+        category: "sfw",
+        description: "Gives you a neko!",
+        usage: "[mention]",
+        example: "@hnxtasia",
+        accessableby: "Members"
+    },
+    run: async (bot, message) => {
+        let mention = message.mentions.users.first();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        if (mention && mention !== message.author) {
+            neko.sfw.nekoGif().then(nekoGif => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} wants you to have some cute cat girls ${mention}.`)
+                    .setImage(nekoGif.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        } else {
+            neko.sfw.nekoGif().then(nekoGif => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`Have some cute cat girls ${message.author}.`)
+                    .setImage(nekoGif.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        }
+    }
+};
