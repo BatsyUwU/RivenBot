a302ab0b07428c571e700c454eac29eaa2eb2ae2
XRzky <marizky65@gmail.com>
2020-05-10 13:20:49 +0700

    commands: added baka
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/sfw/baka.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "baka",
+        aliases: [""],
+        category: "sfw",
+        description: "Gives you a baka!",
+        usage: "[mention]",
+        example: "@hnxtasia",
+        accessableby: "Members"
+    },
+    run: async (bot, message) => {
+        let mention = message.mentions.users.first();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        if (mention && mention !== message.author) {
+            neko.sfw.baka().then(baka => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} says that ${mention} is a baka.`)
+                    .setImage(baka.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        } else {
+            neko.sfw.baka().then(baka => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} is a baka.`)
+                    .setImage(baka.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        }
+    }
+};
