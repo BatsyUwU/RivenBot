fc1909ec11552425ca4961f47b381a25b6d1e185
XRzky <marizky65@gmail.com>
2020-05-10 13:21:53 +0700

    commands: added smug
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/sfw/smug.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "smug",
+        aliases: [""],
+        category: "sfw",
+        description: "Gives you a smug!",
+        usage: "[mention]",
+        example: "@hnxtasia",
+        accessableby: "Members"
+    },
+    run: async (bot, message) => {
+        let mention = message.mentions.users.first();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        if (mention && mention !== message.author) {
+            neko.sfw.smug().then(smug => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} is being smug against ${mention}.`)
+                    .setImage(smug.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        } else {
+            neko.sfw.smug().then(smug => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} is looking a bit smug.`)
+                    .setImage(smug.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        }
+    }
+};
