1cd61667fcc49ba5ea83a8997b204b34213a4ee2
XRzky <marizky65@gmail.com>
2020-05-10 12:01:12 +0700

    command: added djs
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/searches/djs.js
@@ -0,0 +1,31 @@
+const fetch = require('node-fetch');
+
+module.exports = {
+    config: {
+        name: 'djs',
+        aliases: ['discord-js'],
+        category: 'searches',
+        description: 'Searches the DJS docs for whatever you\'d like',
+        usage: '<query> (branch)',
+        example: 'embed setAuthor',
+        accessableby: 'Members'
+    },
+    run: async (bot, message, args) => {
+        const query = args.join(' ');
+        const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
+        
+        fetch(url)
+        .then(res => res.json())
+        .then(embed => {
+            if(embed && !embed.error) {
+                message.channel.send({ embed });
+            } else {
+                message.reply(`I don't know mate, but "${query}" doesn't make any sense!`);
+            }
+        })
+        .catch(e => {
+            console.error(e);
+            message.reply('Darn it! I failed!');
+        })
+    }
+};
