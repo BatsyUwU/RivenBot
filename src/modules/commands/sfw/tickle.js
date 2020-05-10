3b128307bd74fd7cea451db992e936c8d556ba32
XRzky <marizky65@gmail.com>
2020-05-10 13:22:01 +0700

    commands: added tickle
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/sfw/tickle.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "tickle",
+        aliases: [""],
+        category: "sfw",
+        description: "Gives you a tickle!",
+        usage: "[mention]",
+        example: "@hnxtasia",
+        accessableby: "Members"
+    },
+    run: async (bot, message) => {
+        let mention = message.mentions.users.first();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        if (mention && mention !== message.author) {
+            neko.sfw.tickle().then(tickle => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} just tickled ${mention}.`)
+                    .setImage(tickle.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        } else {
+            neko.sfw.tickle().then(tickle => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} has been tickled by me.`)
+                    .setImage(tickle.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        }
+    }
+};
