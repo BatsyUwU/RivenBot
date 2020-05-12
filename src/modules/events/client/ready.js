48112056b58f1eea90f23ee3220e5f1a1c8e73ef
XRzky <marizky65@gmail.com>
2020-05-13 05:01:11 +0700

    events: ready: change bot activities
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/events/client/ready.js
+++ src/modules/events/client/ready.js
@@ -1,12 +1,14 @@
 const { Clients } = require("../../../utils/configs/settings");
-const activities = require("../../../assets/json/activities");
+const { version } = require("../../../../package.json");
 
 module.exports = (client) => {
     console.log(`${client.user.username} is online on ${client.guilds.cache.size} servers!`);
 
+    let activities = [`${Clients.PREFIX}help | v${version}`, 
+    `${client.guilds.cache.size} guilds | ${client.users.cache.size} users`]
     setInterval(function() {
-        let activity = `${Clients.PREFIX}help | ${activities[Math.floor(Math.random() * activities.length)]}`;
+        let activity = `${activities[Math.floor(Math.random() * activities.length)]}`;
         client.user.setActivity(activity, {type: "PLAYING"}); //PLAYING, LISTENING, WATCHING, STREAMING
 
-    }, 300000);
+    }, 150000);
 };
