4aedd6cffd59fe27a2699ea9d241dee5093ed149
XRzky <marizky65@gmail.com>
2020-05-13 05:01:30 +0700

    commands: user: added permissions
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/information/user.js
+++ src/modules/commands/information/user.js
@@ -2,6 +2,7 @@ const { MessageEmbed } = require("discord.js");
 const { Colors, Emotes } = require("../../../utils/configs/settings");
 const { getMember } = require("../../../utils/functions/general");
 const { stripIndents } = require("common-tags");
+const perms = require("../../../assets/json/permissions");
 const moment = require("moment");
 
 module.exports = {
@@ -24,24 +25,27 @@ module.exports = {
             "offline": `${Emotes.OFFLINE} Offline`
         };
 
+        const allowed = Object.entries(member.permissions.serialize()).filter(([perm, allowed]) => allowed).map(([perm]) => `${perms[perm]}`).join(", ");
+
         const roles = member.roles.cache.sort((a, b) => b.position - a.position).filter((r) => r.id !== message.guild.id).map((r) => r.name).join(", ") || "None";
 
         const userEmbed = new MessageEmbed()
             .setColor(member.displayHexColor === "#000000" ? Colors.CUSTOM : member.displayHexColor)
             .setAuthor(`User Information for ${member.user.username}`, member.user.avatarURL({ dynamic: true }))
             .setThumbnail(member.user.displayAvatarURL({ format: "png", dynamic: true, size: 4096 }))
-            .addField("❯ Details", stripIndents`
+            .addField("__Details__", stripIndents`
                 • **Nickname:** ${member.nickname || "None"}
                 • **Tag:** ${member.user.tag}
                 • **ID:** ${member.user.id}
                 • **Status:** ${status[member.user.presence.status]}
-                • **Activity:** ${member.user.presence.activities}`)
-            .addField("❯ Join Dates", stripIndents`
-                • **Server:** ${moment(member.joinedAt).format("ddd, DD MMMM YYYY HH:mm [GMT]Z")}
-                • **Discord:** ${moment(member.user.createdAt).format("ddd, DD MMMM YYYY HH:mm [GMT]Z")}`)
-            .addField(`❯ Roles [${member.roles.cache.filter((f) => f.name !== "@everyone").size}]`, stripIndents`
+                • **Activity:** ${member.user.presence.activities || "None"}`)
+            .addField("__Join Dates__", stripIndents`
+                • **Server:** ${moment(member.joinedAt).format("DD/MM/YYYY HH:mm [GMT]Z")}
+                • **Discord:** ${moment(member.user.createdAt).format("DD/MM/YYYY HH:mm [GMT]Z")}`)
+            .addField(`__Roles [${member.roles.cache.filter((f) => f.name !== "@everyone").size}]__`, stripIndents`
                 • **Highest:** ${member.roles.highest.name}
                 • **List:** ${roles}`)
+            .addField("__Permissions__", allowed ? allowed : "None")
             .setFooter(`Requested by ${message.author.tag}`, message.author.avatarURL({ dynamic: true }))
             .setTimestamp();
 
