04ec24fe1503f331ab78875018565c090f9c9933
XRzky <marizky65@gmail.com>
2020-05-14 05:27:29 +0700

    commands: added some nsfw
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/nsfw/classic.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const { handleValidation } = require("../../../utils/functions/NsfwHandling");
+const Errors = require("../../../utils/functions/errors");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "classic",
+        aliases: [],
+        category: "nsfw",
+        description: "Posts a random classic picture. Warning this commands for 18+",
+        usage: "",
+        example: "",
+        accessableby: "Members"
+    },
+    run: async (client, message, args) => {
+        if (!message.channel.nsfw) return Errors.nsfwAccess(message);
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        message.channel.startTyping();
+        neko.nsfw.classic().then(async img => {
+            const embed = new MessageEmbed()
+                .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                .setImage(img.url)
+                .setFooter("Powered by nekos.life")
+                .setTimestamp();
+            
+            handleValidation(embed, message);
+        });
+        message.channel.stopTyping(true);
+    }
+};
