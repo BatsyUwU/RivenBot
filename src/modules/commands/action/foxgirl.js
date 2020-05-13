a78d2bbc7bf365468dacfdfd489550fb83f206eb
XRzky <marizky65@gmail.com>
2020-05-14 05:40:39 +0700

    commands: added foxgirl
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/modules/commands/action/foxgirl.js
@@ -0,0 +1,45 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../../../utils/configs/settings");
+const client = require("nekos.life");
+const neko = new client();
+
+module.exports = {
+    config: {
+        name: "foxgirl",
+        aliases: [""],
+        category: "action",
+        description: "Gives you a fox girl!",
+        usage: "[mention]",
+        example: "@hnxtasia",
+        accessableby: "Members"
+    },
+    run: async (client, message) => {
+        let mention = message.mentions.users.first();
+
+        const roleColor = message.guild.me.roles.highest.hexColor;
+
+        if (mention && mention !== message.author) {
+            neko.sfw.foxGirl().then(foxGirl => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`${message.author} thinks that ${mention} doesn't have enough fox girls.`)
+                    .setImage(foxGirl.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        } else {
+            neko.sfw.foxGirl().then(foxGirl => {
+                const embed = new MessageEmbed()
+                    .setColor(roleColor === "#000000" ? Colors.CUSTOM : roleColor)
+                    .setDescription(`Have some cute fox girls ${message.author}.`)
+                    .setImage(foxGirl.url)
+                    .setFooter(`Requested by ${message.author.tag} | Powered by nekos.life`, message.author.avatarURL({ dynamic: true }))
+                    .setTimestamp();
+    
+                message.channel.send(embed);
+            });
+        }
+    }
+};
