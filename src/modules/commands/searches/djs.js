2bd73d26b83dedec1476d6697de18758763f485d
XRzky <marizky65@gmail.com>
2020-05-17 14:34:31 +0700

    commands: djs: change to axios
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/searches/djs.js
+++ src/modules/commands/searches/djs.js
@@ -1,31 +1,28 @@
-const fetch = require("node-fetch");
+const axios = require("axios");
 
 module.exports = {
     config: {
         name: "djs",
         aliases: ["discord-js"],
         category: "searches",
-        description: "Searches the DJS docs for whatever you\"d like",
+        description: "Searches the DJS docs for whatever you\'d like",
         usage: "<query> (branch)",
         example: "embed setAuthor",
         accessableby: "Members"
     },
     run: async (client, message, args) => {
         const query = args.join(" ");
-        const url = `https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`;
-        
-        fetch(url)
-        .then(res => res.json())
-        .then(embed => {
-            if(embed && !embed.error) {
+
+        axios.get(`https://djsdocs.sorta.moe/v2/embed?src=stable&q=${encodeURIComponent(query)}`).then((res) => {
+            const embed = res.data;
+            
+            if (embed && !embed.error) {
                 message.channel.send({ embed });
             } else {
-                message.reply(`I don"t know mate, but "${query}" doesn"t make any sense!`);
+                message.reply(`I don't know mate, but "${query}" doesn't make any sense!`)
             }
-        })
-        .catch(e => {
-            console.error(e);
+        }).catch(() => {
             message.reply("Darn it! I failed!");
-        })
+        });
     }
 };
