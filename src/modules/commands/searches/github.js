4c9e67b691b3ee86fae07b8dc38378812e0e03c0
XRzky <marizky65@gmail.com>
2020-05-17 14:34:55 +0700

    commands: github: change to axios
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/commands/searches/github.js
+++ src/modules/commands/searches/github.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require("discord.js");
 const { Colors } = require("../../../utils/configs/settings");
 const Errors = require("../../../utils/functions/errors");
-const fetch = require("node-fetch");
+const axios = require("axios");
 const moment = require("moment");
 
 module.exports = {
@@ -10,60 +10,29 @@ module.exports = {
         aliases: ["git"],
         category: "searches",
         description: "Searches github for a user, organisation or repository",
-        usage: "<users/repos> <username> <repository>",
-        example: "repos XRzky AkiraBot",
+        usage: "<username> [repository]",
+        example: "XRzky",
         accessableby: "Members"
     },
     run: async (client, message, args) => {
         if (!args[0]) {
             return Errors.wrongCmd(message, "github");
-        };
-        
-        if (args[0] === "users") {
-            let username = args[1];
-            if(!username) {
-                return Errors.wrongText(message, "Please provide a valid github account to search.");
-            };
-            
-            fetch(`https://api.github.com/users/${username}`).then((res) => res.json()).then((users) => {
-                if(users.message) {
-                    return Errors.resStatus("404", message, `I wasnt able to find \`${username}\` on the github website!`);
-                }
+        }
 
-                const usersEmbed = new MessageEmbed()
-                    .setColor(Colors.GITHUB)
-                    .setAuthor("GitHub Search Engine", "https://i.imgur.com/4EDz1aY.png", "https://github.com/")
-                    .setTitle(users.login)
-                    .setURL(users.html_url)
-                    .setThumbnail(users.avatar_url)
-                    .setDescription(`${users.bio || "[No bio set]"}`)
-                    .addField("Name", `${users.name || "Not Public."}`, true)
-                    .addField("ID", `${users.id || "Unknown"}`, true)
-                    .addField("Updated", `${moment(users.updated_at, "YYYYMMDDHHmmss").fromNow()}`, true)
-                    .addField("Location", `${users.location || "Invisible."}`, false)
-                    .addField("Repositories", `${users.public_repos}`, true)
-                    .addField("Followers", `${users.followers}`, true)
-                    .addField("Following", `${users.following}`, true)
-                    .addField("Created", `${moment(users.created_at).format("ddd, DD MMMM YYYY HH:mm [GMT]Z")} (${moment(users.created_at, "YYYYMMDDHHmmss").fromNow()})`, false)
-                    .setFooter(`Requested by ${message.author.tag} | Powered by GitHub`, message.author.avatarURL({ dynamic: true }))
-                    .setTimestamp();
-                    
-                message.channel.send(usersEmbed);
-            });
-        } else if (args[0] === "repos") {
-            let user = args[1];
-            if (!user) {
+        if (args[0] && args[1]) {
+            if (!args[0]) {
                 return Errors.wrongText(message, "Please provide the repository owner's username or organisation name.");
-            };
+            }
 
-            let repo = args[2];
-            if (!repo) {
+            if (!args[1]) {
                 return Errors.wrongText(message, "Please provide a repository name to search for.");
             }
 
-            fetch(`https://api.github.com/repos/${user}/${repo}`).then((res) => res.json()).then((repos) => {
+            axios.get(`https://api.github.com/repos/${args[0]}/${args[1]}`).then((res) => {
+                const repos = res.data;
+
                 if(repos.message) {
-                    return Errors.resStatus("404", message, `I wasnt able to find \`${user}/${repo}\` on the github website!`);
+                    return Errors.resStatus("404", message, `I wasnt able to find \`${args[0]}/${args[1]}\` on the github website!`);
                 }
 
                 const reposEmbed = new MessageEmbed()
@@ -82,10 +51,42 @@ module.exports = {
                     .addField("Language", repos.language || "No language", true)
                     .addField("License", repos.license ? repos.license.spdx_id : "Unlicensed", true)
                     .addField("Archived", repos.archived.toString() === "True" ? "Yes" : "No", true)
-                    .setFooter(`Requested by ${message.author.tag} | Powered by GitHub`, message.author.avatarURL({ dynamic: true }))
+                    .setFooter("Powered by GitHub")
                     .setTimestamp();
 
                 message.channel.send(reposEmbed);
+            });
+        } else if (args[0]) {
+            if(!args[0]) {
+                return Errors.wrongText(message, "Please provide a valid github account to search.");
+            };
+            
+            axios.get(`https://api.github.com/users/${args[0]}`).then((res) => {
+                const users = res.data;
+
+                if(users.message) {
+                    return Errors.resStatus("404", message, `I wasnt able to find \`${args[0]}\` on the github website!`);
+                }
+
+                const usersEmbed = new MessageEmbed()
+                    .setColor(Colors.GITHUB)
+                    .setAuthor("GitHub Search Engine", "https://i.imgur.com/4EDz1aY.png", "https://github.com/")
+                    .setTitle(users.login)
+                    .setURL(users.html_url)
+                    .setThumbnail(users.avatar_url)
+                    .setDescription(`${users.bio || "[No bio set]"}`)
+                    .addField("Name", `${users.name || "Not Public."}`, true)
+                    .addField("ID", `${users.id || "Unknown"}`, true)
+                    .addField("Updated", `${moment(users.updated_at, "YYYYMMDDHHmmss").fromNow()}`, true)
+                    .addField("Location", `${users.location || "Invisible."}`, false)
+                    .addField("Repositories", `${users.public_repos}`, true)
+                    .addField("Followers", `${users.followers}`, true)
+                    .addField("Following", `${users.following}`, true)
+                    .addField("Created", `${moment(users.created_at).format("ddd, DD MMMM YYYY HH:mm [GMT]Z")} (${moment(users.created_at, "YYYYMMDDHHmmss").fromNow()})`, false)
+                    .setFooter("Powered by GitHub")
+                    .setTimestamp();
+                    
+                message.channel.send(usersEmbed);
             });
         }
     }
