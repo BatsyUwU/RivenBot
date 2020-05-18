8019fee9ec6579ae404a6d0b5818ff835a241c05
XRzky <marizky65@gmail.com>
2020-05-18 17:03:27 +0700

    RivenBot: Introduce new Embeds functions
    Signed-off-by: XRzky <marizky65@gmail.com>

---
+++ src/utils/functions/Embeds.js
@@ -0,0 +1,23 @@
+const { MessageEmbed } = require("discord.js");
+const { Colors } = require("../configs/settings");
+
+module.exports = {
+    errors: async function (type, message, args) {
+        const embed = new MessageEmbed()
+            .setColor(Colors.RED)
+            .setFooter(message.author.tag, message.author.avatarURL({ dynamic: true }))
+            .setTimestamp();
+        switch (type) {
+            case "noQuery": {
+                embed.setTitle("ERROR!")
+                .setDescription(`💢 **${message.author.tag}**, You must provide a search query!`)
+                break;
+            }
+            default: {
+                embed.setTitle("ERROR!")
+                .setDescription(`💢 **${message.author.tag}**, Sorry, but an error has occured.`)
+            }
+        }
+        message.channel.send(embed);
+    }
+};
