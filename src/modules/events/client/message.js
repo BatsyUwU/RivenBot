897b25921fcd0f083094304ebb6e213b02794230
XRzky <marizky65@gmail.com>
2020-05-25 08:30:08 +0700

    RivenBot: Introduce prefix with mention
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/events/client/message.js
+++ src/modules/events/client/message.js
@@ -5,11 +5,20 @@ module.exports = async (client, message) => {
         return
     };
 
-    if (!message.content.startsWith(Clients.PREFIX)) {
+    const mentionRegex = RegExp(`^<@!${client.user.id}>$`);
+    const mentionRegexPrefix = RegExp(`^<@!${client.user.id}> `);
+
+    if (message.content.match(mentionRegex)) {
+        return message.channel.send(`My prefix for ${message.guild.name} is \`${Clients.PREFIX}\`.`);
+    }
+
+    const prefix = message.content.match(mentionRegexPrefix) ? message.content.match(mentionRegexPrefix)[0] : Clients.PREFIX;
+
+    if (!message.content.startsWith(prefix)) {
         return
     };
 
-    let args = message.content.slice(Clients.PREFIX.length).trim().split(/ +/g);
+    let args = message.content.slice(prefix.length).trim().split(/ +/g);
     let cmd = args.shift().toLowerCase();
 
     let commandFile = client.commands.get(cmd) || client.commands.get(client.aliases.get(cmd));
