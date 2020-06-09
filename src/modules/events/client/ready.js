3d85c3d7ad124d061b87faabd77854d2734c3f19
XRzky <marizky65@gmail.com>
2020-06-10 05:20:21 +0700

    ready: new activity
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/modules/events/client/ready.js
+++ src/modules/events/client/ready.js
@@ -4,10 +4,10 @@ const { version } = require("../../../../package.json");
 module.exports = (client) => {
     console.log(`${client.user.username} is online on ${client.guilds.cache.size} servers!`);
 
-    let activities = [`${Clients.PREFIX}help | v${version}`, 
-    `${client.guilds.cache.size} guilds | ${client.users.cache.size} users`]
+    let activities = [`v${version}`, `${client.guilds.cache.size} servers`, `${client.users.cache.size} users`]
+    let commands = [`${Clients.PREFIX}help`, `${Clients.PREFIX}invite`]
     setInterval(function() {
-        let activity = `${activities[Math.floor(Math.random() * activities.length)]}`;
+        let activity = `${commands[Math.floor(Math.random() * commands.length)]} | ${activities[Math.floor(Math.random() * activities.length)]}`;
         client.user.setActivity(activity, {type: "PLAYING"}); //PLAYING, LISTENING, WATCHING, STREAMING
 
     }, 150000);
