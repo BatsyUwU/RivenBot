15cdd7aa66dbd4f98dcb676b54213153c70812af
XRzky <marizky65@gmail.com>
2020-06-10 05:19:46 +0700

    command: delete command logs
    Signed-off-by: XRzky <marizky65@gmail.com>

--- src/handlers/command.js
+++ src/handlers/command.js
@@ -1,26 +1,15 @@
 const { readdirSync } = require("fs");
-const ascii = require("ascii-table");
-
-let table = new ascii("Commands");
-table.setHeading("Command", "Status");
 
 module.exports = (client) => {
 	const load = (dirs) => {
 		const commands = readdirSync(`./src/modules/commands/${dirs}/`).filter((file) => file.endsWith(".js"));
 		for (let file of commands) {
 			let pull = require(`../modules/commands/${dirs}/${file}`);
-			if (pull.config.name) {
-                client.commands.set(pull.config.name, pull);
-                table.addRow(file, "✅");
-            } else {
-                table.addRow(file, "❌  -> missing a help.name, or help.name is not a string.");
-                continue;
-            }
+			client.commands.set(pull.config.name, pull);
 			if (pull.config.aliases) {
 				pull.config.aliases.forEach((a) => client.aliases.set(a, pull.config.name));
 			}
 		}
 	};
 	["action", "administrator", "fun", "information", "miscellaneous", "moderation", "music", "nsfw", "owner", "searches", "utilities"].forEach((x) => load(x));
-	console.log(table.toString());
 };
